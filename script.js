542739bc3d1052fa44501d9b2e5169765a8dca3e
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-04-20 22:53:59 +0530

    added hovering logic

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-async function displayChart() {
+async function getData() {
     const response = await fetch('data.json');
     if (!response.ok) {
         throw new Error('JSON not found');
     }
     const data = await response.json();
-    const canvas = $('#myCanvas').get(0);
+    return data;
+}
+
+async function displayChart(canvas) {
+    const jsonData = await getData();
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "red";
     const barWidth = 40;
     const spacing = 20;
     let x = 0;
     let y = 0;
-    data.forEach((e) => {
-        ctx.fillRect(x, (canvas.height - e.amount - 90), barWidth, e.amount+80);
+    jsonData.forEach((e) => {
+        ctx.fillRect(x, (canvas.height - e.amount - 100), barWidth, e.amount + 80);
         ctx.textBaseline = 'alphabetic';
         ctx.fillText(e.day, x + 10, canvas.height);
         x += barWidth + spacing;
     })
 }
 
-displayChart();
+async function handleMouseMove(canvas, event) {
+    if (!event) return;
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    console.log(`${mouseX}, ${mouseY}`);
+    const jsonData = await getData();
+    const barWidth = 40;
+    const spacing = 20;
+    let x = 0;
+    const ctx = canvas.getContext("2d");
+    jsonData.forEach((e) => {
+        let rectX = x;
+        let rectY = canvas.height - e.amount - 100;
+        let rectWidth = barWidth;
+        let rectHeight = e.amount + 80;
+        if (mouseX >= rectX && mouseX <= rectX + rectWidth && mouseY >= rectY && mouseY <= rectY + rectHeight) {
+            ctx.fillStyle = "blue";
+        } else {
+            ctx.fillStyle = "red";
+        }
+        ctx.fillRect(x, (canvas.height - e.amount - 100), barWidth, e.amount + 80);
+        ctx.textBaseline = 'alphabetic';
+        x += barWidth + spacing;
+    })
+
+}
+
+const canvas = $('#myCanvas').get(0);
+window.addEventListener('load', () => displayChart(canvas));
+canvas.addEventListener('mousemove', (event) => handleMouseMove(canvas, event));
