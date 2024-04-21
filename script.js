b5978399ec4dfddbe52400d74a0ed3bf32997221
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-04-21 19:34:25 +0530

    added logic for rectangle hovering

--- script.js
+++ script.js
@@ -11,14 +11,19 @@ async function displayChart(canvas) {
     const jsonData = await getData();
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "red";
+    ctx.strokeStyle = "red";
     const barWidth = 40;
     const spacing = 20;
-    let x = 0;
+    let x = 10;
     let y = 0;
     jsonData.forEach((e) => {
-        ctx.fillRect(x, (canvas.height - e.amount - 100), barWidth, e.amount + 80);
+        ctx.beginPath();
+        ctx.roundRect(x, (canvas.height - e.amount - 100), barWidth, e.amount + 80, [3]);
+        ctx.fill()
+        ctx.stroke();
+        ctx.font = "12px Sans-Serif";
         ctx.textBaseline = 'alphabetic';
-        ctx.fillText(e.day, x + 10, canvas.height);
+        ctx.fillText(e.day, x + 7, canvas.height);
         x += barWidth + spacing;
     })
 }
@@ -28,11 +33,11 @@ async function handleMouseMove(canvas, event) {
     const rect = canvas.getBoundingClientRect();
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
-    console.log(`${mouseX}, ${mouseY}`);
+    // console.log(`${mouseX}, ${mouseY}`);
     const jsonData = await getData();
     const barWidth = 40;
     const spacing = 20;
-    let x = 0;
+    let x = 10;
     const ctx = canvas.getContext("2d");
     jsonData.forEach((e) => {
         let rectX = x;
@@ -40,13 +45,27 @@ async function handleMouseMove(canvas, event) {
         let rectWidth = barWidth;
         let rectHeight = e.amount + 80;
         if (mouseX >= rectX && mouseX <= rectX + rectWidth && mouseY >= rectY && mouseY <= rectY + rectHeight) {
-            ctx.fillStyle = "blue";
+            ctx.fillStyle = "white";
+            ctx.strokeStyle = "black";
+            ctx.beginPath();
+            ctx.roundRect(x - 5, canvas.height - e.amount - 150, barWidth + 10, 30, [3]);
+            ctx.fill();
+            ctx.stroke();
+            ctx.textBaseline = 'bottom';
+            ctx.fillStyle = "black";
+            ctx.fillText(e.amount, x + 4, canvas.height - e.amount - 127);
         } else {
             ctx.fillStyle = "red";
+            ctx.strokeStyle = "red";
+            // extra dimensions to ensure stroke borders are also wiped out
+            ctx.clearRect(x - 6, canvas.height - e.amount - 151, barWidth + 12, 32);
         }
-        ctx.fillRect(x, (canvas.height - e.amount - 100), barWidth, e.amount + 80);
-        ctx.textBaseline = 'alphabetic';
+        ctx.beginPath();
+        ctx.roundRect(x, (canvas.height - e.amount - 100), barWidth, e.amount + 80, [3]);
+        ctx.fill();
+        ctx.stroke();
         x += barWidth + spacing;
+        ctx.stroke();
     })
 
 }
